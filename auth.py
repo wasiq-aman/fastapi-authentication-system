b53e9be165f5973a0a9ee84880a325fb59b4f6a0
Wasiq Aman <111419988+wasiq-aman@users.noreply.github.com>
2025-01-17 14:22:00 +0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,39 +1,7 @@
-# import jwt # type: ignore
-# from datetime import datetime, timedelta
-# import os
-# from dotenv import load_dotenv # type: ignore
-
-# # Load environment variables
-# load_dotenv()
-
-# # Secret key and algorithm
-# SECRET_KEY = os.getenv("SECRET_KEY")
-# ALGORITHM = "HS256"
-
-# # Function to create JWT token
-# def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=30)):
-#     to_encode = data.copy()
-#     expire = datetime.utcnow() + expires_delta
-#     to_encode.update({"exp": expire})
-#     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-#     return encoded_jwt
-
-# # Function to verify JWT token
-# def verify_token(token: str):
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         return payload
-#     except jwt.ExpiredSignatureError:
-#         return None
-#     except jwt.JWTError:
-#         return None
-
-
-
-import jwt
 from datetime import datetime, timedelta
 import os
-from dotenv import load_dotenv
+import jwt # type: ignore
+from dotenv import load_dotenv # type: ignore
 
 # Load environment variables
 load_dotenv()
@@ -43,7 +11,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 
 # Function to create JWT token
-def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=30)):
+def create_access_token(data: dict, expires_delta: timedelta = timedelta(hours=1)):
     to_encode = data.copy()
     expire = datetime.utcnow() + expires_delta
     to_encode.update({"exp": expire})
